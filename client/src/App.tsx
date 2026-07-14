861c0a671dc4bd95bce107ad2fb237ac246b4eb6
edanesedemircan <github_edanesedemircan81@gmail.com>
2026-07-14 16:42:22 +0300

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { CreateWall } from './pages/CreateWall';
